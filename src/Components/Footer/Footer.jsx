0bf1bd66f2137e15fa9f48f2075fd57803d825e5
Inder-designer <bhupinder7814@gmail.com>
2024-09-12 17:47:12 +0530

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,59 +8,59 @@ import indeed from "../../assets/icons/in.png";
 const Footer = () => {
   return (
     <div className="">
-      <div className="pt-10 sm:pt-14 lg:pt-16 rounded-t-[36px] bg-white">
+      <div className="pt-8 lg:rounded-t-[36px] bg-white">
         <div className="container mx-auto">
           <div className="pb-8 pt-4 xs:pb-11 sm:pb-16 md:pb-20 lg:pb-[120px] relative overflow-hidden">
-            <div className="xs:flex flex-wrap pb-4 sm:pb-10 lg:pb-14 border-b-[1.6px] border-border1">
+            <div className="xs:flex flex-wrap pb-8 border-b-[1.6px] border-border1">
               <div className="w-full xs:w-1/2 lg:w-[30%] 2xl:w-1/4 mb-5 xs:mb-0">
-                <p className="text-sm sm:text-base md:text-lg text-text2 mb-4 xs:mb-6 md:mb-10">
+                <p className="text-sm sm:text-base text-text2 mb-4 xs:mb-6">
                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                   Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
                   natoque .
                 </p>
                 <div>
-                  <p className="text-sm sm:text-base md:text-lg text-text2">
+                  <p className="text-sm sm:text-base text-text2">
                     +152 78457349587
                   </p>
-                  <p className="text-sm sm:text-base md:text-lg text-text2">
+                  <p className="text-sm sm:text-base text-text2">
                     property@gmail.com
                   </p>
                 </div>
               </div>
               <div className="w-full xs:w-1/2 lg:w-2/5 2xl:w-1/2 2xl:px-36 xl:pl-24 xl:pr-16 lg:pl-14 lg:pr-12 md:pl-20 sm:pl-10 xs:pl-6 xs:pr-4 ">
                 <ul className="grid grid-rows-5 grid-flow-col *:duration-300 xs:justify-between">
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Home
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Buy
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Rent
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Commercial
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       New Project
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Find Agent
                     </Link>
                   </li>
-                  <li className="hover:translate-x-1 mb-2 xs:mb-3 md:mb-4 lg:mb-6">
-                    <Link className="text-sm sm:text-base md:text-lg text-text2 *:duration-300 hover:text-primary">
+                  <li className="hover:translate-x-1 mb-2 xs:mb-3">
+                    <Link className="text-sm sm:text-base text-text2 *:duration-300 hover:text-primary">
                       Explore
                     </Link>
                   </li>
@@ -89,7 +89,7 @@ const Footer = () => {
               </div>
             </div>
             <div className="text-center absolute w-full">
-              <h4 className="text-[36px] xs:text-[48px] sm:text-[68px] md:text-[90px] lg:text-[130px] uppercase text-primary opacity-[8%]">
+              <h4 className="text-[36px] xs:text-[48px] sm:text-[68px] md:text-[90px] lg:text-[130px] uppercase text-primary opacity-[20%]">
                 REAL ESTATE
               </h4>
             </div>
